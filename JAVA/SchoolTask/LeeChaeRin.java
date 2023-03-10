7c8275daabd6221e387d1e5da602e4b635d92e11
Chaerin Lee <124489210+ddooott@users.noreply.github.com>
2023-03-10 10:04:01 +0900

    Create LeeChaeRin.java

---
+++ JAVA/SchoolTask/LeeChaeRin.java
@@ -0,0 +1,7 @@
+public class LeeChaeRin {
+  public static void main (String arg[]){
+      System.out.println("Oriented Object Programming");
+      System.out.println("Lee Chae Rin");
+      System.out.println("202221075");
+  }
+}
